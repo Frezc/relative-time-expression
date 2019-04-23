213e6b4c98328a82cb326124f20c95b70fdaedbc
frezc <504021398@qq.com>
2019-04-23 23:05:46 +0800

    fix error msg and test

--- packages/core/__tests__/parse.spec.ts
+++ packages/core/__tests__/parse.spec.ts
@@ -36,15 +36,15 @@ describe('parse', () => {
   })
 
   it('throw error when token match failed', () => {
-    expect(() => parse(' no - d')).toThrow('unknown token \'n\' at (1, 2)');
+    expect(() => parse(' no - d')).toThrow('unknown token \`n\` at (1, 2)');
   });
 
   it('throw error when forget operator', () => {
-    expect(() => parse(' now  1d')).toThrow('expect operator(+, -, /, \\) but found 1 at (6, 7)');
+    expect(() => parse(' now  1d')).toThrow('expect operator(+, -, /, \\) but found \`1\` at (6, 7)');
   });
 
   it('throw error when forget unit', () => {
-    expect(() => parse(' now +1+d')).toThrow('expect unit(e.g. s, m, h, d, ...) but found + at (7, 8)');
+    expect(() => parse(' now +1+d')).toThrow('expect unit(e.g. s, m, h, d, ...) but found \`+\` at (7, 8)');
   });
 
   it('throw end of input error when stop after operator in offset', () => {
